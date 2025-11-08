224c70abb85e77ce072c64a9350bdba560fc0208
cyxabima <ukashaanwerali@gmail.com>
2025-11-08 16:01:23 +0500

    ADDED:run-from-function

--- app/Huffman/decompressing/huffman_decompressor.py
+++ app/Huffman/decompressing/huffman_decompressor.py
@@ -5,7 +5,7 @@ from app.Huffman.fileHandler.files import read_compressed_file
 def byte_to_bits(byte_data: str):
     bits = ""
     for byte in byte_data:
-        ## Fomatting, like 3-> 00000011 to 8 bits (from 1 byte)
+        # Formatting, like 3-> 00000011 to 8 bits (from 1 byte)
         bits += format(byte, "08b")
     return bits
 
@@ -15,17 +15,17 @@ def huff_decompress_file(path: str):
     root = create_huffman_tree(freq_list)
     bitString = byte_to_bits(compressed_bytes)
     if padding > 0:
-        ## removing extra padding added to complete byte while encoding
+        # removing extra padding added to complete byte while encoding
         bitString = bitString[:-padding]
 
     decoded_bytes = []
     current_node = root
     for bit in bitString:
         current_node = current_node.left if bit == "0" else current_node.right
-        ## Reached EndPoint
+        # Reached EndPoint
         if current_node.left is None and current_node.right is None:
             decoded_bytes.append(current_node.byte)
-            current_node = root  ## Reset
+            current_node = root  # Reset
 
     with open("Decompressed.txt", "wb") as f:
         f.write(bytes(decoded_bytes))
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import sys
 from .Huffman.fileHandler.files import (
     get_frequency_list,
     generate_codes,
@@ -7,21 +8,23 @@ from .Huffman.fileHandler.files import (
 from .Huffman.huffman_main.hufman import create_huffman_tree
 from .Huffman.decompressing.huffman_decompressor import huff_decompress_file
 
+
+def run_from_electron():
+    if len(sys.argv) < 3:
+        raise Exception("too less arguments")
+    file = sys.argv[2]
+    if sys.argv[1] == "-c":
+        data = read_file_as_bytes(file)
+        frequency = get_frequency_list(data=data)
+        root = create_huffman_tree(frequency)
+        codes = generate_codes(root)
+        encoded_bitstring = "".join(codes[char] for char in data)
+        write_compressed_file("output.rau", encoded_bitstring, frequency)
+        sys.stdout.flush()  # IMPORTANT: Makes sure the output is sent right away
+    elif sys.argv[1] == "-d":
+        huff_decompress_file(file)
+        print("Decompressing Done")
+
+
 if __name__ == "__main__":
-    print("RAU: Compressor Tool")
-    data = read_file_as_bytes("input.txt")
-    # print (data)
-    frequency = get_frequency_list(data=data)
-    print(sum(frequency))
-    root = create_huffman_tree(frequency)
-    print(root.frequency)
-    # print(generate_codes(root))
-    codes = generate_codes(root)
-
-    encoded_bitstring = "".join(codes[char] for char in data)
-    # print(encoded_bitstring)
-
-    write_compressed_file("output.rau", encoded_bitstring, frequency)
-
-    huff_decompress_file("output.rau")
-    print("Decompressing Done")
+    run_from_electron()
