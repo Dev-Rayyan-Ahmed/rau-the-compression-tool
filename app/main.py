f81deeeb0edfaa77cecaee38c15677916fdccb26
cyxabima <ukashaanwerali@gmail.com>
2025-11-24 08:58:53 +0500

    Update: outputpath

--- app/main.py
+++ app/main.py
@@ -7,7 +7,12 @@ from .huffman.decompressing.huffman_decompressor import huff_decompress_file
 def run_from_electron():
     if len(sys.argv) < 2:
         raise Exception("too less arguments")
-    output_path = sys.argv[3] if len(sys.argv) > 3 else os.getcwd()
+    if len(sys.argv) > 3:
+        output_path = sys.argv[3]
+
+    else:
+        output_path = os.path.dirname(os.path.abspath(sys.argv[2]))
+
     file = sys.argv[2]
     if sys.argv[1] == "-c":
         compress_file(file, output_path)
